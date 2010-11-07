b62533844fa9caf57567e9a18ebd1236a30721c3
hadley <h.wickham@gmail.com>
2010-11-07 15:36:11 -0600

    Remove ggplot2 remnants

--- R/hex.r
+++ R/hex.r
@@ -1,61 +1,4 @@
-StatBinhex <- proto(Stat, {
-  objname <- "binhex"
-  desc <- "Bin 2d plane into hexagons"
-  
-  calculate <- function(., data, scales, binwidth = NULL, bins = 30, na.rm = FALSE, ...) {
-    try_require("hexbin")
-    data <- remove_missing(data, na.rm, c("x", "y"), name="stat_hexbin")
-
-    if (is.null(binwidth)) {
-      binwidth <- c( 
-        diff(scales$x$input_set()) / bins,
-        diff(scales$y$input_set() ) / bins
-      )
-    }
-    
-    hexBin(data$x, data$y, binwidth)
-  }
-  
-  seealso <- list(
-    "stat_bin2d" = "For rectangular binning"
-  )
-  
-  default_aes <- function(.) aes(fill = ..count..)
-  required_aes <- c("x", "y")
-  default_geom <- function(.) GeomHex
-  
-  examples <- function() {
-    d <- ggplot(diamonds, aes(carat, price))
-    d + stat_binhex()
-    d + geom_hex()
-    
-    # You can control the size of the bins by specifying the number of
-    # bins in each direction:
-    d + stat_binhex(bins = 10)
-    d + stat_binhex(bins = 30)
-    
-    # Or by specifying the width of the bins
-    d + stat_binhex(binwidth = c(1, 1000))
-    d + stat_binhex(binwidth = c(.1, 500))
-    
-    # With qplot
-    qplot(x, y, data = diamonds, geom="hex", xlim = c(4, 10), ylim = c(4, 10))
-    qplot(x, y, data = diamonds, geom="hex", xlim = c(4, 10), ylim = c(4, 10),
-      binwidth = c(0.1, 0.1))
-  }
-  
-})
-
-# Bin 2d plane into hexagons
-# Wrapper around \code{\link[hexbin]{hcell2xy}} that returns a data frame
-# 
-# @arguments x positions
-# @arguments y positions
-# @arguments numeric vector of length 2 giving binwidth in x and y directions
-# @keyword internal
-hexBin <- function(x, y, binwidth) {
-  try_require("hexbin")
-  
+hex_bin <- function(x, y, binwidth = hex_binwidth(range(x), range(y))) {
   # Convert binwidths into bounds + nbins
   xbnds <- c(
     round_any(min(x), binwidth[1], floor) - 1e-6, 
@@ -81,4 +24,11 @@ hexBin <- function(x, y, binwidth) {
     count = hb@count, 
     density = hb@count / sum(hb@count, na.rm=TRUE)
   )
+}
+
+hex_binwidth <- function(xrange, yrange, bins) {
+   c( 
+     diff(xrange) / bins,
+     diff(yrange) / bins
+   )
 }
