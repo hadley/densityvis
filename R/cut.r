e919d54870d371f362f7f38157f5ffe8869b5260
hadley <h.wickham@gmail.com>
2010-11-07 16:24:43 -0600

    Move cut code into own file

---
+++ R/cut.r
@@ -0,0 +1,33 @@
+#' Discretise continuous variable, equal interval length.
+#' Cut numeric vector into intervals of equal length.
+#' 
+#' @param x numeric vector
+#' @param n number of intervals to create, OR
+#' @param length of each interval
+#' @param ... other arguments passed on to \code{\link{cut}}
+#' @keywords manip
+#' @seealso \code{\link{cut_number}}
+#' @examples 
+#' table(cut_interval(1:100, n = 10))
+#' table(cut_interval(1:100, n = 11))
+#' table(cut_interval(1:100, length = 10))
+cut_interval <- function(x, n = NULL, length = NULL, ...) {
+  cut(x, interval_breaks(x, nbins = n, binwidth = length), 
+    include.lowest = TRUE, ...)
+}
+
+#' Discretise continuous variable, equal number of points.
+#' Cut numeric vector into intervals containing equal number of points.
+#' 
+#' @param x numeric vector
+#' @param n number of intervals to create
+#' @param ... other arguments passed on to \code{\link{cut}}
+#' @keywords manip
+#' @seealso \code{\link{cut_interval}}
+#' @examples 
+#' table(cut_number(runif(1000), n = 10))
+cut_number <- function(x, n = NULL, ...) {
+  probs <- seq(0, 1, length = n + 1)
+  qs <- quantile(x, probs, na.rm = TRUE)
+  cut(x, qs, include.lowest = TRUE, ...)
+}
--- R/interval.r
+++ R/interval.r
@@ -67,36 +67,3 @@ interval_breaks <- function(x, nbins = 30, binwidth=NULL, origin=NULL, range=NUL
 }
 
 
-# Discretise continuous variable, equal interval length.
-# Cut numeric vector into intervals of equal length.
-# 
-# @arguments numeric vector
-# @arguments number of intervals to create, OR
-# @arguments length of each interval
-# @arguments other arguments passed on to \code{\link{cut}}
-# @keyword manip
-# @seealso \code{\link{cut_number}}
-# 
-#X table(cut_interval(1:100, n = 10))
-#X table(cut_interval(1:100, n = 11))
-#X table(cut_interval(1:100, length = 10))
-cut_interval <- function(x, n = NULL, length = NULL, ...) {
-  cut(x, interval_breaks(x, nbins = n, binwidth = length), 
-    include.lowest = TRUE, ...)
-}
-
-# Discretise continuous variable, equal number of points.
-# Cut numeric vector into intervals containing equal number of points.
-# 
-# @arguments numeric vector
-# @arguments number of intervals to create, OR
-# @arguments length of each interval
-# @arguments other arguments passed on to \code{\link{cut}}
-# @keyword manip
-# @seealso \code{\link{cut_interval}}
-#X table(cut_number(runif(1000), n = 10))
-cut_number <- function(x, n = NULL, ...) {
-  probs <- seq(0, 1, length = n + 1)
-  qs <- quantile(x, probs, na.rm = TRUE)
-  cut(x, qs, include.lowest = TRUE, ...)
-}
