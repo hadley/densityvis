8e33a1afd573ddb757014d7bcac0e857d7766231
hadley <h.wickham@gmail.com>
2010-11-07 16:22:31 -0600

    Remove redundant breaks code

--- R/interval.r
+++ R/interval.r
@@ -34,13 +34,13 @@ interval_bin <- function(x, weight = NULL, breaks = interval_breaks(x), drop = F
 #' Calculate breaks for interval (1d) bins
 #'
 #' @export
-interval_breaks <- function(x, binwidth=NULL, origin=NULL, range=NULL, right = TRUE, width=0.9) {
+interval_breaks <- function(x, nbins = 30, binwidth=NULL, origin=NULL, range=NULL, right = TRUE, width=0.9) {
   
   if (is.null(range)) {
     range <- range(x, na.rm = TRUE, finite = TRUE)
   }
   if (is.null(binwidth)) {
-    binwidth <- diff(range) / 30
+    binwidth <- diff(range) / nbins
   }
   
   # If x is an integer, place breaks between values
@@ -81,7 +81,8 @@ interval_breaks <- function(x, binwidth=NULL, origin=NULL, range=NULL, right = T
 #X table(cut_interval(1:100, n = 11))
 #X table(cut_interval(1:100, length = 10))
 cut_interval <- function(x, n = NULL, length = NULL, ...) {
-  cut(x, breaks(x, "width", n, length), include.lowest = TRUE, ...)
+  cut(x, interval_breaks(x, nbins = n, binwidth = length), 
+    include.lowest = TRUE, ...)
 }
 
 # Discretise continuous variable, equal number of points.
@@ -95,30 +96,7 @@ cut_interval <- function(x, n = NULL, length = NULL, ...) {
 # @seealso \code{\link{cut_interval}}
 #X table(cut_number(runif(1000), n = 10))
 cut_number <- function(x, n = NULL, ...) {
-  cut(x, breaks(x, "n", n), include.lowest = TRUE, ...)
+  probs <- seq(0, 1, length = n + 1)
+  qs <- quantile(x, probs, na.rm = TRUE)
+  cut(x, qs, include.lowest = TRUE, ...)
 }
-
-breaks <- function(x, equal, nbins = NULL, binwidth = NULL) {
-  equal <- match.arg(equal, c("numbers", "width"))
-  if ((!is.null(nbins) && !is.null(binwidth)) || (is.null(nbins) && is.null(binwidth))) {
-    stop("Specify exactly one of n and width")
-  }
-  
-  rng <- range(x, na.rm = TRUE, finite = TRUE)
-  if (equal == "width") {
-    if (!is.null(binwidth)) {
-      fullseq(rng, binwidth)
-    } else {
-      seq(rng[1], rng[2], length = nbins + 1)
-    }
-  } else {
-    if (!is.null(binwidth)) {
-      probs <- seq(0, 1, by = binwidth)
-    } else {
-      probs <- seq(0, 1, length = nbins + 1)
-    }
-    quantile(x, probs, na.rm = TRUE)
-  }
-  
-}
-
