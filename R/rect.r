6fca786bbb1c23e9ea03cf48d61b39ca91128587
hadley <h.wickham@gmail.com>
2010-11-11 20:15:44 -0600

    Get rect_bin working

--- R/rect.r
+++ R/rect.r
@@ -1,7 +1,9 @@
 #' Bin data into rectangles (2d)
 #'
 #' @export
-rect_bin <- function(x, y, weight, scales, breaks = rect_breaks(x, y), drop = TRUE, na.rm = FALSE) {
+#' bins <- rect_bin(runif(1000), runif(1000))
+#' ggplot(bins) + geom_rect(aes(xmin = left, xmax = right, ymin = bottom, ymax = top, fill = count))
+rect_bin <- function(x, y, weight = rep.int(1, length(x)), breaks = rect_breaks(x, y), drop = FALSE, na.rm = FALSE) {
     
   # If !na.rm, remove missing values with a warning.  
   # Otherwise just remove them
@@ -22,20 +24,20 @@ rect_bin <- function(x, y, weight, scales, breaks = rect_breaks(x, y), drop = TR
   x <- x[!missing & weight > 0]
   y <- y[!missing & weight > 0]
   
-
   xn <- length(breaks$x)
   yn <- length(breaks$y)
   xbin <- findInterval(x, breaks$x, all.inside = TRUE)
   ybin <- findInterval(y, breaks$y, all.inside = TRUE)
-    
-  bin <- (xbin - 1) + (ybin - 1) * xn
-  count <- vaggregate(weight, bin, sum, na.rm = TRUE, .default = 0)
-
+  
+  bin <- (xbin - 1L) + (ybin - 1L) * (xn - 1L) + 1L
+  count <- vaggregate(weight, bin, sum, na.rm = TRUE, 
+    .default = 0, .n = (xn - 1L) * (yn - 1L))
+  
   data.frame(
-    top =    rep(breaks$y[-yn], by = xn), 
-    bottom = rep(breaks$y[-1],  by = xn), 
-    left =   rep(breaks$x[-xn], times = yn),
-    right =  rep(breaks$x[-1],  times = yn),
+    top =    rep(breaks$y[-yn], xn - 1), 
+    bottom = rep(breaks$y[-1],  xn - 1), 
+    left =   rep(breaks$x[-xn], each = yn - 1),
+    right =  rep(breaks$x[-1],  each = yn - 1),
     count = count
   )
 }
@@ -43,40 +45,45 @@ rect_bin <- function(x, y, weight, scales, breaks = rect_breaks(x, y), drop = TR
 #' Calculate breaks for rectangular (2d) bins
 #'
 #' @export
-rect_breaks <- function(x, y, range, binwidth = NULL, bins = NULL, origin = NULL) {
+rect_breaks <- function(x, y, range = NULL, binwidth = NULL, bins = 20, origin = NULL) {
+  if (is.null(range)) {
+    range <- list(x = range(x, na.rm = TRUE), y = range(y, na.rm = TRUE ))
+  }
+  
   # Determine binwidth, if omitted
   if (is.null(binwidth)) {
     binwidth <- c(NA, NA)
-    if (is.integer(data$x)) {
+    if (is.integer(x)) {
       binwidth[1] <- 1
     } else {
       binwidth[1] <- diff(range$x) / bins
     }
-    if (is.integer(data$y)) {
+    if (is.integer(y)) {
       binwidth[2] <- 1
     } else {
       binwidth[2] <- diff(range$y) / bins
-    }      
+    }  
+    message("Using binwidth (", format(binwidth[1], digits = 3), ", ", 
+      format(binwidth[2], digits = 3), ")")    
   }
   stopifnot(is.numeric(binwidth))
   stopifnot(length(binwidth) == 2)
   
-  # Determine breaks, if omitted
-  if (is.null(breaks)) {
-    if (is.null(origin)) {
-      breaks <- list(
-        fullseq(range$x, binwidth[1]),
-        fullseq(range$y, binwidth[2])
-      )
-    } else {
-      breaks <- list(
-        seq(origin[1], max(range$x) + binwidth[1], binwidth[1]),
-        seq(origin[2], max(range$y) + binwidth[2], binwidth[2])
-      )
-    }
+  if (is.null(origin)) {
+    breaks <- list(
+      fullseq(range$x, binwidth[1]),
+      fullseq(range$y, binwidth[2])
+    )
+  } else {
+    breaks <- list(
+      seq(origin[1], max(range$x) + binwidth[1], binwidth[1]),
+      seq(origin[2], max(range$y) + binwidth[2], binwidth[2])
+    )
   }
-  stopifnot(is.list(breaks))
-  stopifnot(length(breaks) == 2)
-  stopifnot(all(sapply(breaks, is.numeric)))
+  # stopifnot(is.list(breaks))
+  # stopifnot(length(breaks) == 2)
+  # stopifnot(all(sapply(breaks, is.numeric)))
+
   names(breaks) <- c("x", "y")
+  breaks
 }
